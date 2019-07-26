90269d944e81873242bd2ded243763c3791aa356
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2019-07-26 19:48:59 -0400

    repo init

---
+++ LambdaExpression/src/main/java/com/coderunners/lambda/Example1.java
@@ -0,0 +1,16 @@
+package com.coderunners.lambda;
+
+/**
+ * Write a program using Stream terminal operation (matchAll()) 
+ * to find if all candidates appearing for the interview are local natives. 
+ * Also retrieve the youngest candidate details using (min() method)
+ * 
+ * @author Vishal Patel
+ */
+public class Example1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Kem cho majama ne");
+	}
+}
